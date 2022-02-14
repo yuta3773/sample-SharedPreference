f4e336fecdd3854ec287088afe8872195b819e3e
神塚佑太 <cybor_ggg@icloud.com>
2022-02-14 22:29:43 +0900

    SharedPreferencesの保存処理を実装

--- SharedPreference/app/src/main/java/com/example/sharedpreference/MainActivity.kt
+++ SharedPreference/app/src/main/java/com/example/sharedpreference/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.sharedpreference
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
+import com.example.sharedpreference.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //デフォルトの共有プリファレンスを習得
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        //共有プリファレンス習得した値を代入
+        val inputText = pref.getString("INPUT_TEXT", "")
+        //値の表示
+        binding.editText.setText(inputText)
+
+        //タップされた時にリスナーをセット
+        binding.button.setOnClickListener { onSaveTapped() }
+    }
+    //保存処理
+    private fun onSaveTapped() {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        pref.edit {
+            putString("INPUT_TEXT", binding.editText.text.toString())
+        }
     }
 }
